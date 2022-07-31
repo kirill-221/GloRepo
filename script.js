c280c144b43b8bda10f73b5faf79446d556a5292
Kirill Butorin <butorink-00@yandex.ru>
2022-07-31 16:39:38 +0300

    Выполнение второго задания

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-let title, screens, screenPrice, rollback, fullPrice, adaptive;
+let title = 'Название проекта ';
+let screens = 'Простые, Сложные, Интерактивные';
+let screenPrice = 2000;
+let rollback = 2;
+let fullPrice = 1000;
+let adaptive = true;
 
-alert('Мой первый alert!');
-console.log('Hello world');
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
 
-title = 'Название проекта ';
+console.log(screens.length);
+
+console.log(screenPrice);
+console.log(fullPrice);
+
+console.log(screens.toLowerCase, screens.split());
+
+console.log(fullPrice * (rollback / 100));
