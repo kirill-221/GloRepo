cfc815d13406c21301e35db87ee88738ce452880
Kirill Butorin <butorink-00@yandex.ru>
2022-08-02 16:19:39 +0300

    Добавление условий

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+'use strict';
 let title = prompt('Как называется ваш проект?');
 let screens = prompt(
     'Какие типы экранов нужно разработать?',
@@ -12,5 +13,16 @@ let servicePrice2 = +prompt('Сколько это будет стоить?');
 let rollback = 2;
 let fullPrice = screenPrice + servicePrice1 + servicePrice2;
 console.log(fullPrice);
-let servicePercentPrice = fullPrice * (rollback / 100);
-console.log(servicePercentPrice);
+let percentIntermediary = fullPrice * (rollback / 100);
+let servicePercentPrice = fullPrice - percentIntermediary;
+console.log(Math.ceil(servicePercentPrice));
+
+if (fullPrice >= 30000) {
+    console.log('Даем скидку в 10%');
+} else if (fullPrice >= 15000 && fullPrice < 30000) {
+    console.log('Даем скидку в 5%');
+} else if (fullPrice < 15000 && fullPrice >= 0) {
+    console.log('Скидка не предусмотрена');
+} else if (fullPrice < 0) {
+    console.log('Что то пошло не так');
+}
